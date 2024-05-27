1295076cf828b76720d6caccc2ddeff8fb84b5f9
Marta <mjbylica@gmail.com>
2024-05-27 14:08:37 +0200

    adding swapi tests

---
+++ tests/basic_api_tests/swapi_test.py
@@ -0,0 +1,18 @@
+import requests
+import pytest
+
+
+@pytest.fixture()
+def get_han_solo_data():
+    response = requests.get("https://swapi.dev/api/people/14")
+    return response
+
+
+def test_han_solo_is_in_empire_strikes_back(get_han_solo_data):
+    data = get_han_solo_data.json()
+    assert data["name"] == "Han Solo"
+    assert "https://swapi.dev/api/films/9/" in data["films"]
+
+
+
+
